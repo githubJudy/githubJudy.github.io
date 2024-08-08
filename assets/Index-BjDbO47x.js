45c88f376a50b16920c0fcdaa3610ff38a9d918d
judy <judy@example.com>
2024-08-08 21:09:31 +0800

    20240808 fix

--- assets/Index-BjDbO47x.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,a,b as c,o as s,r as n}from"./index-CiWuUuAa.js";const r={__name:"Index",setup(_){function e(){n.push("/home02")}return(d,p)=>(s(),t("div",null,[a(" 11 "),c("button",{onClick:e},"22")]))}},l=o(r,[["__scopeId","data-v-6d414c63"]]);export{l as default};
